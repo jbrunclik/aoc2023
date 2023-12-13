b2cdf771ca32d03382a09b568dfb66b6acbad950
jbrunclik <jiri.brunclik@productboard.com>
2023-12-13 16:29:53 +0100

    Day 12 Part 2 solution using cache

--- Day12/Day12.cs
+++ Day12/Day12.cs
@@ -2,68 +2,114 @@ namespace Aoc2023.Day12;
 
 partial class Day12 : Solution
 {
-    List<SpringCondition> ParseInput() =>
-        Input
-            .Select(line =>
-            {
-                var lineSplit = line.Split(" ", 2);
-                var records = lineSplit[0];
-                var damagedGroups = lineSplit[1].Split(",").Select(int.Parse).ToList();
-                return new SpringCondition(records, damagedGroups);
-            })
-            .ToList();
-
-    void GenerateCombinations(string records, int index, ref List<String> variants)
+    List<(string, List<int>)> ParseInput(bool unfold)
     {
-        var unknownIndex = records.IndexOf('?', index);
-        if (unknownIndex == -1)
+        var springs = new List<(string, List<int>)>();
+        foreach (var line in Input)
         {
-            variants.Add(records);
-            return;
+            var lineParts = line.Split(" ", 2);
+            var records = lineParts[0];
+            var groups = lineParts[1];
+
+            if (unfold)
+            {
+                records = string.Join('?', Enumerable.Repeat(records, 5));
+                groups = string.Join(',', Enumerable.Repeat(groups, 5));
+            }
+
+            springs.Add((records, groups.Split(",").Select(int.Parse).ToList()));
+        }
+        return springs;
+    }
+
+    long ComputeValidArrangements(
+        string records,
+        List<int> groups,
+        int recordIndex,
+        int groupIndex,
+        int groupLength,
+        ref Dictionary<(int, int, int), long> cache
+    )
+    {
+        var key = (recordIndex, groupIndex, groupLength);
+        if (cache.ContainsKey(key))
+            return cache[key];
+
+        if (recordIndex == records.Length)
+        {
+            if (groupIndex == groups.Count && groupLength == 0)
+                return 1;
+            else if (groupIndex == groups.Count - 1 && groups[groupIndex] == groupLength)
+                return 1;
+            else
+                return 0;
         }
 
-        var withDot =
-            records.Substring(0, unknownIndex) + "." + records.Substring(unknownIndex + 1);
-        var withHash =
-            records.Substring(0, unknownIndex) + "#" + records.Substring(unknownIndex + 1);
+        var arrangements = 0L;
+        foreach (var variant in new char[] { '.', '#' })
+        {
+            if (records[recordIndex] == variant || records[recordIndex] == '?')
+            {
+                if (variant == '.' && groupLength == 0)
+                    arrangements += ComputeValidArrangements(
+                        records,
+                        groups,
+                        recordIndex + 1,
+                        groupIndex,
+                        0,
+                        ref cache
+                    );
+                else if (
+                    variant == '.'
+                    && groupLength > 0
+                    && groupIndex < groups.Count
+                    && groups[groupIndex] == groupLength
+                )
+                    arrangements += ComputeValidArrangements(
+                        records,
+                        groups,
+                        recordIndex + 1,
+                        groupIndex + 1,
+                        0,
+                        ref cache
+                    );
+                else if (variant == '#')
+                    arrangements += ComputeValidArrangements(
+                        records,
+                        groups,
+                        recordIndex + 1,
+                        groupIndex,
+                        groupLength + 1,
+                        ref cache
+                    );
+            }
+        }
 
-        GenerateCombinations(withDot, unknownIndex + 1, ref variants);
-        GenerateCombinations(withHash, unknownIndex + 1, ref variants);
+        cache[key] = arrangements;
+        return arrangements;
     }
-
-    List<string> GenerateVariants(string records)
-    {
-        var variants = new List<string>();
-        GenerateCombinations(records, 0, ref variants);
-        return variants;
-    }
-
-    List<int> CountHahshes(string records) =>
-        records
-            .Select((c, index) => new { Char = c, Index = index })
-            .GroupBy(
-                x =>
-                    new { x.Char, IndexDiff = x.Index - records.Take(x.Index).Count(y => y == '#') }
-            )
-            .Where(g => g.Key.Char == '#')
-            .Select(g => g.Count())
-            .ToList();
-
-    bool IsValidVariant(string records, List<int> damagedGroups) =>
-        CountHahshes(records).SequenceEqual(damagedGroups);
 
     public override string Part1()
     {
-        var springConditions = ParseInput();
-        return springConditions
-            .SelectMany(
-                s => GenerateVariants(s.Records).Where(v => IsValidVariant(v, s.DamagedGroups))
-            )
-            .Count()
-            .ToString();
+        var springs = ParseInput(false);
+        var sum = 0L;
+        foreach (var (records, groups) in springs)
+        {
+            var cache = new Dictionary<(int, int, int), long>();
+            sum += ComputeValidArrangements(records, groups, 0, 0, 0, ref cache);
+        }
+        return sum.ToString();
     }
 
-    public override string Part2() => "";
-
-    record SpringCondition(string Records, List<int> DamagedGroups);
+    public override string Part2()
+    {
+        var springs = ParseInput(true);
+        var sum = 0L;
+        foreach (var (records, groups) in springs)
+        {
+            var cache = new Dictionary<(int, int, int), long>();
+            sum += ComputeValidArrangements(records, groups, 0, 0, 0, ref cache);
+        }
+        return sum.ToString();
+    }
 }
