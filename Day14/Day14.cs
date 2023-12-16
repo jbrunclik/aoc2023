062913a210cdeec8e18ccc44b7d88f6eebd8b61a
jbrunclik <jiri.brunclik@productboard.com>
2023-12-16 14:04:48 +0100

    Day 14 Part 2 solution using cycle detection

--- Day14/Day14.cs
+++ Day14/Day14.cs
@@ -8,12 +8,16 @@ partial class Day14 : Solution
         var cols = Input[0].Length;
 
         var map = new char[rows, cols];
-        Input
-            .Select(
-                (line, rowIndex) =>
-                    line.Select((ch, colIndex) => map[rowIndex, colIndex] = ch).ToArray()
-            )
-            .ToArray();
+        for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+        {
+            for (int colIndex = 0; colIndex < cols; colIndex++)
+            {
+                char ch = Input[rowIndex][colIndex];
+                if (ch == '#' || ch == 'O')
+                    map[rowIndex, colIndex] = ch;
+            }
+        }
+
         return map;
     }
 
@@ -21,12 +25,12 @@ partial class Day14 : Solution
     {
         var tiltedPlatform = new char[platform.GetLength(0), platform.GetLength(1)];
 
-        // Copy all cube-shaped rocks and empty space
+        // Copy all cube-shaped rocks
         for (var row = 0; row < platform.GetLength(0); row++)
         {
             for (var col = 0; col < platform.GetLength(1); col++)
             {
-                if (platform[row, col] == '#' || platform[row, col] == '.')
+                if (platform[row, col] == '#')
                 {
                     tiltedPlatform[row, col] = platform[row, col];
                 }
@@ -51,13 +55,29 @@ partial class Day14 : Solution
 
                         newRow--;
                     }
-                    tiltedPlatform[row, col] = '.'; // Replace with empty space
-                    tiltedPlatform[newRow, col] = 'O'; // Move to the new place
+                    tiltedPlatform[newRow, col] = 'O';
                 }
             }
         }
 
         return tiltedPlatform;
+    }
+
+    char[,] Rotate(char[,] platform)
+    {
+        int rows = platform.GetLength(0);
+        int cols = platform.GetLength(1);
+
+        char[,] rotated = new char[cols, rows];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                rotated[j, rows - 1 - i] = platform[i, j];
+            }
+        }
+
+        return rotated;
     }
 
     int ComputeLoad(char[,] platform)
@@ -75,6 +95,22 @@ partial class Day14 : Solution
         return load;
     }
 
+    static string PlatformToString(char[,] platform) =>
+        string.Join(
+            "",
+            Enumerable
+                .Range(0, platform.GetLength(0))
+                .Select(
+                    row =>
+                        new string(
+                            Enumerable
+                                .Range(0, platform.GetLength(1))
+                                .Select(col => platform[row, col])
+                                .ToArray()
+                        )
+                )
+        );
+
     public override string Part1()
     {
         var platform = ParseInput();
@@ -83,5 +119,29 @@ partial class Day14 : Solution
         return load.ToString();
     }
 
-    public override string Part2() => "";
+    public override string Part2()
+    {
+        var platform = ParseInput();
+        var seen = new Dictionary<string, int>();
+
+        var totalIterations = 1_000_000_000;
+        for (var i = 0; i < totalIterations; i++)
+        {
+            var k = PlatformToString(platform);
+            if (!seen.TryGetValue(k, out int firstOccurence))
+                seen[k] = i;
+            else
+            {
+                var cycleLength = i - firstOccurence;
+                var repeatCycle = (totalIterations - i) / cycleLength;
+                i += cycleLength * repeatCycle;
+            }
+
+            for (var j = 0; j < 4; j++)
+                platform = Rotate(Tilt(platform));
+        }
+
+        var load = ComputeLoad(platform);
+        return load.ToString();
+    }
 }
